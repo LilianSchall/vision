0c4ec2658de9f974b40e8e8998c326e40afa6411
Lilian Schall <lilian.schall@epita.fr>
2023-06-21 00:02:03 +0200

    feat: making example evolve with the engine functionnality

--- examples/square/src/main.rs
+++ examples/square/src/main.rs
@@ -1,5 +1,10 @@
-use vision::rendering::init;
+use vision::constraints::Vector;
+use vision::math::{Vector2, Vector3};
+use vision::rendering::{Camera, Engine, init};
 
 fn main() {
-    init("Square: Vision", (800, 600));
+    let canvas = init("Square: Vision", (800, 600));
+    let mut engine: Engine<'static> = Engine::new(canvas, 800, 600);
+    engine.add_centered_camera(Vector2::ZERO);
+    engine.run();
 }
