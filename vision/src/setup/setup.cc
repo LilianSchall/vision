47fbb9c21e3a81d6d4cedd7ef9a1fff9952a37de
Lilian Schall <lilian.schall@epita.fr>
2023-07-15 14:41:21 +0000

    fix: removed some confusions about public exposure of properties in CMakeLists, adapted the add of SDL2 library

--- vision/src/setup/setup.cc
+++ vision/src/setup/setup.cc
@@ -10,7 +10,9 @@ Init::Init(std::string title, int width, int height) {
                                     SDL_WINDOWPOS_CENTERED,
                                     width, height,
                                     0);
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window,
+                                                -1,
+                                                SDL_RENDERER_ACCELERATED);
 
     this->window = std::unique_ptr<SDL_Window, SDL_WindowDestroyer>(window);
     this->renderer = std::unique_ptr<SDL_Renderer, SDL_RendererDestroyer>(renderer);
