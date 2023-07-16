7a29905edfe66a5696aeb71f992bbcd2332f8d39
Lilian Schall <lilian.schall@epita.fr>
2023-07-17 01:47:04 +0200

    feat: added some directions vectors that will be updated when rotating a transform

--- vision/src/transform/transform.cc
+++ vision/src/transform/transform.cc
@@ -1,6 +1,8 @@
 #include "transform.hh"
 
-Transform::Transform(Vector3& _position, Vector3& _rotation) : position(_position), rotation(_rotation) {}
+Transform::Transform(Vector3& _position, Vector3& _rotation, Vector3 &_size)
+: position(_position), rotation(_rotation), size(_size){
+}
 
 void Transform::translate(Vector3 &t) {
     this->position += t;
--- vision/src/transform/transform.hh
+++ vision/src/transform/transform.hh
@@ -1,16 +1,51 @@
 #ifndef TRANSFORM_HH
 #define TRANSFORM_HH
 
-#include "maths/maths.hh"
+#include "maths/vector/vector3/vector3.hh"
 
 class Transform {
 
 public:
-    Transform(Vector3 &_position, Vector3 &_rotation);
+    Transform(Vector3 &_position, Vector3 &_rotation, Vector3 &_size);
     void translate(Vector3& t);
 
+    inline Vector3 up() {
+        return _up;
+    }
+
+    inline Vector3 down() {
+        return _down;
+    }
+
+    inline Vector3 left() {
+        return _left;
+    }
+
+    inline Vector3 right() {
+        return _right;
+    }
+
+    inline Vector3 forward() {
+        return _forward;
+    }
+
+    inline Vector3 backward() {
+        return _backward;
+    }
+
+
+public:
     Vector3 position;
     Vector3 rotation;
+    Vector3 size;
+
+private:
+    Vector3 _up = Vector3(0, 1, 0);
+    Vector3 _down = Vector3(0, -1, 0);
+    Vector3 _left = Vector3(-1, 0, 0);
+    Vector3 _right = Vector3(1, 0, 0);
+    Vector3 _forward = Vector3(0, 0, 1);
+    Vector3 _backward = Vector3(0, 0, -1);
 };
 
 #endif // !TRANSFORM_HH
