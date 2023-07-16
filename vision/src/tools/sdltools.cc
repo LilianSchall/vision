f09f9e521c083c73e7b6903c222748b50fe893d6
Lilian Schall <lilian.schall@epita.fr>
2023-07-17 01:47:54 +0200

    feat: added a function to map an rgb value into an uint32

--- vision/src/tools/sdltools.cc
+++ vision/src/tools/sdltools.cc
@@ -2,8 +2,19 @@
 
 void SDL_WindowDestroyer::operator()(SDL_Window *window) const {
     SDL_DestroyWindow(window);
+    std::cout << "SDL_Window has been successfully destroyed.\n";
 }
 
 void SDL_RendererDestroyer::operator()(SDL_Renderer *renderer) const {
     SDL_DestroyRenderer(renderer);
+    std::cout << "SDL_Renderer has been successfully destroyed.\n";
+}
+
+void SDL_TextureDestroyer::operator()(SDL_Texture *texture) const {
+    SDL_DestroyTexture(texture);
+    std::cout << "SDL_Texture has been successfully destroyed.\n";
+}
+
+uint32_t map_rgb(uint32_t r, uint32_t g, uint32_t b) {
+    return 0xFF000000 | (r << 16) | (g << 8) | b;
 }
--- vision/src/tools/sdltools.hh
+++ vision/src/tools/sdltools.hh
@@ -2,6 +2,7 @@
 #define SDLTOOLS_HH
 
 #include <SDL2/SDL.h>
+#include <iostream>
 
 struct SDL_WindowDestroyer {
     void operator()(SDL_Window *window) const;
@@ -10,5 +11,11 @@ struct SDL_WindowDestroyer {
 struct SDL_RendererDestroyer {
     void operator()(SDL_Renderer *renderer) const;
 };
+
+struct SDL_TextureDestroyer {
+    void operator()(SDL_Texture *texture) const;
+};
+
+uint32_t map_rgb(uint32_t r, uint32_t g, uint32_t b);
 
 #endif // !SDLTOOLS_HH
