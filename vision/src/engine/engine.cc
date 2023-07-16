bfac9326bb59075b68d3f0585ce2057c4ae5c6ad
Lilian Schall <lilian.schall@epita.fr>
2023-07-17 01:53:32 +0200

    feat: added a destructor to engine class in order to properly quit SDL, integrated the camera system to the engine running method

--- vision/src/engine/engine.cc
+++ vision/src/engine/engine.cc
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "engine.hh"
 
-Engine::Engine(std::string title, int width, int height) {
+Engine::Engine(const std::string& title, int width, int height) {
 
     this->_width = width;
     this->_height = height;
+
+    SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window *_window = SDL_CreateWindow(title.c_str(),
                                     SDL_WINDOWPOS_CENTERED,
@@ -19,23 +21,40 @@ Engine::Engine(std::string title, int width, int height) {
     this->renderer = std::unique_ptr<SDL_Renderer, SDL_RendererDestroyer>(_renderer);
 }
 
+void Engine::add_camera(Camera &cam) {
+
+    cam.create_texture(this->renderer.get());
+    cameras.push_back(&cam);
+}
+
 int Engine::run() const {
 
     SDL_Event event;
     bool running = true;
 
-    while (running) {
+    while(running){
         if (SDL_PollEvent(&event)) {
             // we wrap the polling of event in a condition
             // so that we do not send an event to handle
             // that has been already processed
             handle_keyboard_event(&event, &running);
         }
-        SDL_SetRenderDrawColor(this->renderer.get(), 0,0,0,0);
-        SDL_RenderClear(this->renderer.get());
-        SDL_RenderPresent(this->renderer.get());
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000 / 60));
-    }
 
+        // we iterate over all cameras and render its POV
+        for (Camera *cam : cameras) {
+            cam->render(this->renderer.get());
+        }
+
+        // after rendering all POVs from the camera, we present the frame to the user
+        SDL_RenderPresent(this->renderer.get());
+#if 0
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000/ 60));
+#endif
+    }
     return 0;
+}
+
+Engine::~Engine() {
+    SDL_Quit();
+    std::cout << "Engine has been successfully destroyed. SDL_Quit() triggered.\n";
 }
