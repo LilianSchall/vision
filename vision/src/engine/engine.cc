79b2f54fdc8302ec4c31bd921f19fbae8cbb3736
Lilian Schall <lilian.schall@epita.fr>
2023-07-17 17:58:11 +0200

    feat: divided the run logic of Engine into two separate thread which are controlled by a boolean

--- vision/src/engine/engine.cc
+++ vision/src/engine/engine.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "engine.hh"
 
-Engine::Engine(const std::string& title, int width, int height) {
+Engine::Engine(const std::string &title, int width, int height) {
 
     this->_width = width;
     this->_height = height;
@@ -9,16 +9,21 @@ Engine::Engine(const std::string& title, int width, int height) {
     SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window *_window = SDL_CreateWindow(title.c_str(),
-                                    SDL_WINDOWPOS_CENTERED,
-                                    SDL_WINDOWPOS_CENTERED,
-                                    width, height,
-                                    0);
+                                           SDL_WINDOWPOS_CENTERED,
+                                           SDL_WINDOWPOS_CENTERED,
+                                           width, height,
+                                           0);
     SDL_Renderer *_renderer = SDL_CreateRenderer(_window,
-                                                -1,
-                                                SDL_RENDERER_ACCELERATED);
+                                                 -1,
+                                                 SDL_RENDERER_ACCELERATED);
 
     this->window = std::unique_ptr<SDL_Window, SDL_WindowDestroyer>(_window);
     this->renderer = std::unique_ptr<SDL_Renderer, SDL_RendererDestroyer>(_renderer);
+}
+
+
+void Engine::add_object(Object &obj) {
+    objects.push_back(&obj);
 }
 
 void Engine::add_camera(Camera &cam) {
@@ -27,30 +32,49 @@ void Engine::add_camera(Camera &cam) {
     cameras.push_back(&cam);
 }
 
-int Engine::run() const {
 
+void task_logic(bool *running) {
     SDL_Event event;
-    bool running = true;
 
-    while(running){
+    while (*running) {
         if (SDL_PollEvent(&event)) {
             // we wrap the polling of event in a condition
             // so that we do not send an event to handle
             // that has been already processed
-            handle_keyboard_event(&event, &running);
+            handle_keyboard_event(&event, running);
+            handle_sdl_event(&event, running);
         }
-
-        // we iterate over all cameras and render its POV
-        for (Camera *cam : cameras) {
-            cam->render(this->renderer.get());
-        }
-
-        // after rendering all POVs from the camera, we present the frame to the user
-        SDL_RenderPresent(this->renderer.get());
 #if 0
         std::this_thread::sleep_for(std::chrono::milliseconds(1000/ 60));
 #endif
     }
+}
+
+void task_render(bool *running, SDL_Renderer *renderer, std::list<Object*> *objects, std::list<Camera*> *cameras) {
+
+    while (*running) {
+        // we iterate over all cameras and render its POV
+        for (Camera *cam: *cameras) {
+            cam->render(renderer, objects);
+        }
+
+        // after rendering all POVs from the camera, we present the frame to the user
+        SDL_RenderPresent(renderer);
+        std::cout << "Render..\n";
+    }
+
+}
+
+int Engine::run() {
+
+    bool running = true;
+
+    std::jthread thread_logic{task_logic, &running};
+    std::jthread thread_render{task_render, &running, renderer.get(), &objects, &cameras};
+
+    thread_logic.join();
+    thread_render.join();
+
     return 0;
 }
 
